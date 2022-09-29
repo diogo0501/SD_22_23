13a85f60f567ded4103c4c9269e502323ef1f436
Miguel <56080093+migas342@users.noreply.github.com>
2022-09-29 15:44:03 +0100

    Add files via upload

---
+++ include/node-private.h
@@ -0,0 +1,90 @@
+#ifndef _NODE_PRIVATE_H
+#define _NODE_PRIVATE_H
+
+#include "tree-private.h"
+
+
+/* Função para adicionar um par chave-valor à árvore.
+ * Os dados de entrada desta função deverão ser copiados, ou seja, a
+ * função vai *COPIAR* a key (string) e os dados para um novo espaço de
+ * memória que tem de ser reservado. Se a key já existir na árvore,
+ * a função tem de substituir a entrada existente pela nova, fazendo
+ * a necessária gestão da memória para armazenar os novos dados.
+ * Retorna 0 (ok) ou -1 em caso de erro.
+ */
+void node_destroy(struct node_t *treeRoot);
+/* Função para adicionar um par chave-valor à árvore.
+ * Os dados de entrada desta função deverão ser copiados, ou seja, a
+ * função vai *COPIAR* a key (string) e os dados para um novo espaço de
+ * memória que tem de ser reservado. Se a key já existir na árvore,
+ * a função tem de substituir a entrada existente pela nova, fazendo
+ * a necessária gestão da memória para armazenar os novos dados.
+ * Retorna 0 (ok) ou -1 em caso de erro.
+ */
+int node_put(struct node_t *treeRoot, struct entry_t *entry);
+
+
+/* Função para adicionar um par chave-valor à árvore.
+ * Os dados de entrada desta função deverão ser copiados, ou seja, a
+ * função vai *COPIAR* a key (string) e os dados para um novo espaço de
+ * memória que tem de ser reservado. Se a key já existir na árvore,
+ * a função tem de substituir a entrada existente pela nova, fazendo
+ * a necessária gestão da memória para armazenar os novos dados.
+ * Retorna 0 (ok) ou -1 em caso de erro.
+ */
+struct entry_t *node_get(struct node_t *treeRoot, char *key);
+
+/* Função para adicionar um par chave-valor à árvore.
+ * Os dados de entrada desta função deverão ser copiados, ou seja, a
+ * função vai *COPIAR* a key (string) e os dados para um novo espaço de
+ * memória que tem de ser reservado. Se a key já existir na árvore,
+ * a função tem de substituir a entrada existente pela nova, fazendo
+ * a necessária gestão da memória para armazenar os novos dados.
+ * Retorna 0 (ok) ou -1 em caso de erro.
+ */
+int node_calculateTreeHeight(struct node_t *treeRoot);
+
+/* Função para adicionar um par chave-valor à árvore.
+ * Os dados de entrada desta função deverão ser copiados, ou seja, a
+ * função vai *COPIAR* a key (string) e os dados para um novo espaço de
+ * memória que tem de ser reservado. Se a key já existir na árvore,
+ * a função tem de substituir a entrada existente pela nova, fazendo
+ * a necessária gestão da memória para armazenar os novos dados.
+ * Retorna 0 (ok) ou -1 em caso de erro.
+ */
+struct node_t *node_findLeftmostLeaf(struct node_t *node);
+/* Função para adicionar um par chave-valor à árvore.
+ * Os dados de entrada desta função deverão ser copiados, ou seja, a
+ * função vai *COPIAR* a key (string) e os dados para um novo espaço de
+ * memória que tem de ser reservado. Se a key já existir na árvore,
+ * a função tem de substituir a entrada existente pela nova, fazendo
+ * a necessária gestão da memória para armazenar os novos dados.
+ * Retorna 0 (ok) ou -1 em caso de erro.
+ */
+int node_del(struct tree_t *tree,struct node_t *treeRoot, char *key);
+
+/* Função para adicionar um par chave-valor à árvore.
+ * Os dados de entrada desta função deverão ser copiados, ou seja, a
+ * função vai *COPIAR* a key (string) e os dados para um novo espaço de
+ * memória que tem de ser reservado. Se a key já existir na árvore,
+ * a função tem de substituir a entrada existente pela nova, fazendo
+ * a necessária gestão da memória para armazenar os novos dados.
+ * Retorna 0 (ok) ou -1 em caso de erro.
+ */
+char **node_getKeys(struct tree_t *tree,struct node_t *treeRoot, char **keys, int i);
+
+/* Função para adicionar um par chave-valor à árvore.
+ * Os dados de entrada desta função deverão ser copiados, ou seja, a
+ * função vai *COPIAR* a key (string) e os dados para um novo espaço de
+ * memória que tem de ser reservado. Se a key já existir na árvore,
+ * a função tem de substituir a entrada existente pela nova, fazendo
+ * a necessária gestão da memória para armazenar os novos dados.
+ * Retorna 0 (ok) ou -1 em caso de erro.
+ */
+void **node_getValues(struct tree_t *tree,struct node_t *treeRoot, void **values, int i);
+
+/* Função auxiliar max.
+ */
+int max(int a, int b);
+
+#endif
