83cdc8b49ea34cfe951b29cc1ca4e66b70814f2d
imtunapotato <fc56334@alunos.fc.ul.pt>
2022-10-02 19:17:04 +0100

    Update serialization.c

--- source/serialization.c
+++ source/serialization.c
@@ -1,5 +1,24 @@
 #include "../include/serialization.h"
+#include <string.h>
+#include <stdlib.h>
 
-int keyArray_to_buffer(char **keys, char **keys_buf);
+int keyArray_to_buffer(char **keys, char **keys_buf){
+    int size = strlen(*keys)+1;
+    keys_buf = malloc(sizeof(int)+size);
+    if(keys_buf==NULL){
+        return -1;
+    }
 
-char** buffer_to_keyArray(char *keys_buf, int keys_buf_size)
+    memcpy(keys_buf,&size,sizeof(int));
+    memcpy(keys_buf+sizeof(int),&keys,size);
+    return(strlen(*keys_buf));
+}
+
+char** buffer_to_keyArray(char *keys_buf, int keys_buf_size){
+    char** keys;
+    int size= strlen(keys_buf-sizeof(int)+1);
+    keys= malloc(size);
+    char* keysFromBuffer= keys_buf+sizeof(int);
+    memcpy(keys,&keysFromBuffer,size);
+    return keys;
+}
