28373192ae8dbeb3dcaa5ec856825cab503b57a4
Miguel <56080093+migas342@users.noreply.github.com>
2022-10-07 17:19:07 +0100

    Update tree.c

--- source/tree.c
+++ source/tree.c
@@ -13,6 +13,9 @@ Miguel Santos, fc54461
 #include "../include/entry.h"
 #include "../include/node-private.h"
 
+/* Função para criar uma nova árvore tree vazia.
+ * Em caso de erro retorna NULL.
+ */
 struct tree_t *tree_create() {
 
 	//Criação e alocação da struct tree_t
@@ -37,6 +40,8 @@ struct tree_t *tree_create() {
 
 }
 
+/* Função para libertar toda a memória ocupada por uma árvore.
+ */
 void tree_destroy(struct tree_t *tree) {
 
 	if(tree == NULL) {
@@ -65,6 +70,14 @@ void tree_destroy(struct tree_t *tree) {
 	free(tree);
 }
 
+/* Função para adicionar um par chave-valor à árvore.
+ * Os dados de entrada desta função deverão ser copiados, ou seja, a
+ * função vai *COPIAR* a key (string) e os dados para um novo espaço de
+ * memória que tem de ser reservado. Se a key já existir na árvore,
+ * a função tem de substituir a entrada existente pela nova, fazendo
+ * a necessária gestão da memória para armazenar os novos dados.
+ * Retorna 0 (ok) ou -1 em caso de erro.
+ */
 int tree_put(struct tree_t *tree, char *key, struct data_t *value) {
 
 	//Retorna -1 em caso de erro na tree, key ou value
@@ -88,12 +101,22 @@ int tree_put(struct tree_t *tree, char *key, struct data_t *value) {
 		return code;
 	}
 
+	if(code == 0) {
 	//No caso de sucesso, o numero de elementos é incrementado
 	tree->nrElements++;
+	}
 
 	return 0;
 }
 
+/* Função para obter da árvore o valor associado à chave key.
+ * A função deve devolver uma cópia dos dados que terão de ser
+ * libertados no contexto da função que chamou tree_get, ou seja, a
+ * função aloca memória para armazenar uma *CÓPIA* dos dados da árvore,
+ * retorna o endereço desta memória com a cópia dos dados, assumindo-se
+ * que esta memória será depois libertada pelo programa que chamou
+ * a função. Devolve NULL em caso de erro.
+ */
 struct data_t *tree_get(struct tree_t *tree, char *key) {
 
 	//Retorna null em caso de erro na tree ou na key
@@ -115,6 +138,10 @@ struct data_t *tree_get(struct tree_t *tree, char *key) {
 	return dupedData;
 }
 
+/* Função para remover um elemento da árvore, indicado pela chave key,
+ * libertando toda a memória alocada na respetiva operação tree_put.
+ * Retorna 0 (ok) ou -1 (key not found).
+ */
 int tree_del(struct tree_t *tree, char *key) {
 
 	//Retorna -1 em caso de erro na tree ou na key
@@ -133,14 +160,22 @@ int tree_del(struct tree_t *tree, char *key) {
 	return isDeleted;
 }
 
+/* Função que devolve o número de elementos contidos na árvore.
+ */
 int tree_size(struct tree_t *tree) {
 	return tree->nrElements;
 }
 
+/* Função que devolve a altura da árvore.
+ */
 int tree_height(struct tree_t *tree) {
 	return node_calculateTreeHeight(tree->root);
 }
 
+/* Função que devolve um array de char* com a cópia de todas as keys da
+ * árvore, colocando o último elemento do array com o valor NULL e
+ * reservando toda a memória necessária. As keys devem vir ordenadas segundo a ordenação lexicográfica das mesmas.
+ */
 char **tree_get_keys(struct tree_t *tree) {
 
 	//Retorna null em caso de erro
@@ -174,6 +209,10 @@ char **tree_get_keys(struct tree_t *tree) {
 	return keys;
 }
 
+/* Função que devolve um array de void* com a cópia de todas os values da
+ * árvore, colocando o último elemento do array com o valor NULL e
+ * reservando toda a memória necessária.
+ */
 void **tree_get_values(struct tree_t *tree) {
 
 	//Retorna null em caso de erro na tree
@@ -193,6 +232,8 @@ void **tree_get_values(struct tree_t *tree) {
 	return values;
 }
 
+/* Função que liberta toda a memória alocada por tree_get_keys().
+ */
 void tree_free_keys(char **keys) {
 
 	char *string = keys[0];
@@ -211,6 +252,8 @@ void tree_free_keys(char **keys) {
 
 }
 
+/* Função que liberta toda a memória alocada por tree_get_values().
+ */
 void tree_free_values(void **values) {
 
 	void *data = values[0];
