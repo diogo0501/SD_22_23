3a2655606d6fa04e71d430b72bff6c7fb2d3b5d2
Miguel <56080093+migas342@users.noreply.github.com>
2022-10-07 17:17:02 +0100

    Update tree-private.h

--- include/tree-private.h
+++ include/tree-private.h
@@ -1,13 +1,16 @@
 #ifndef _TREE_PRIVATE_H
 #define _TREE_PRIVATE_H
 
-#include "../include/tree.h"
-
+#include "tree.h"
+/* Struct node_t representa um nodulo cujo conteudo eh uma entry e referencias para os seus filhos
+ */
 struct node_t {
 	struct entry_t *entry;
 	struct node_t *left, *right;
 };
 
+/* Struct tree_t representa uma arvore composta por elementos da estrutura node_t
+ */
 struct tree_t {
 	struct node_t *root;
 	int nrElements;
