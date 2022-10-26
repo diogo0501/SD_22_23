30332f97bcd5f0f182184f32d9755d3f03bb9c59
Diogo Fernandes <diogoalex2001@hotmail.com>
2022-10-26 16:45:01 +0100

    Just saving

--- source/message-private.c
+++ source/message-private.c
@@ -12,35 +12,12 @@ Miguel Santos, fc54461
 #include <sys/types.h>
 #include <sys/socket.h>
 
-/* 
- * TODO
- */
-// int send_all(int sock, uint8_t *buf, int len) {
-//     char *ptr = (char *)buf;
-//     int total = 0;
-//     int bytesleft = len;
-//     int n;
-//     while (total < len)
-//     {
-//         {
-//             n = send(sock, buf + total, bytesleft, 0);
-//             if (n == -1)
-//             {
-//                 break;
-//             }
-//             total += n;
-
-//             bytesleft -= n;
-//         }
-//     }
-//     return n == -1 ? -1 : total; //Retorna a totalidade dos bytes enviados
-// }
 
 int send_all(int sock, uint8_t *buf, int len) {
 
     int bufsize = len;
 
-    while(len>0) {
+    while(len > 0) {
 
         int res = write(sock, buf, len);
         if(res < 0) {
@@ -55,35 +32,10 @@ int send_all(int sock, uint8_t *buf, int len) {
     return bufsize;
 }
 
-/* 
- * TODO
- */
-// int recv_all(int sock, uint8_t* buf, unsigned len) {
-//     char *ptr = (char *)buf;
-//     int total = 0;
-//     int bytesleft = len;
-//     int n;
-//     while (total < len)
-//     {
-//         {
-//             n = recv(sock, buf + total, bytesleft, 0);
-//             if (n == -1 || n == 0)
-//             {
-//                 n = -1;
-//                 break;
-//             }
-//             total += n;
-
-//             bytesleft -= n;
-//         }
-//     }
-//     return n == -1 ? -1 : total; //Retorna a totalidade dos bytes enviados
-// }
-
 int recv_all(int sock, uint8_t* buf, unsigned len) {
     int bufsize = len;
 
-    while(len>0) {
+    while(len > 0) {
 
         int res = read(sock, buf, len);
 
